1da3c7090a4680d29d93226e77fb40dbf28fa0bd
Manuel Ospina <mospina@localhost.localdomain>
2021-03-01 05:59:47 +0900

    Exercise 5.1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React, { useState, useEffect } from 'react'
-import Blog from './components/Blog'
-import Login from './components/Login'
-import blogService from './services/blogs'
+import React, { useState, useEffect } from "react";
+import Blog from "./components/Blog";
+import Login from "./components/Login";
+import Notification from "./components/Notification";
+import blogService from "./services/blogs";
+import loginService from "./services/login";
 
 const App = () => {
-  const [blogs, setBlogs] = useState([])
+  const [blogs, setBlogs] = useState([]);
+  const [user, setUser] = useState(null);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  const handleLogin = async (credential) => {
+    try {
+      const user = await loginService.login(credential);
+      setUser(user);
+    } catch (exception) {
+      setErrorMessage("Wrong Credentials");
+      setTimeout(() => {
+        setErrorMessage(null);
+      }, 5000);
+    }
+  };
 
   useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
-  }, [])
+    blogService.getAll().then((blogs) => setBlogs(blogs));
+  }, []);
 
-  return (
-    <div>
-      <h2>blogs</h2>
-      <Login />
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
-    </div>
-  )
-}
+  if (user === null) {
+    return (
+      <div>
+        <Notification message={errorMessage} />
+        <Login handleLogin={handleLogin} />
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <h2>blogs</h2>
+        <Notification message={errorMessage} />
+        {blogs.map((blog) => (
+          <Blog key={blog.id} blog={blog} />
+        ))}
+      </div>
+    );
+  }
+};
 
-export default App
+export default App;
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,27 +1,39 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
-const Login = () => {
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
+const Login = ({ handleLogin }) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-  const handleLogin = (event) => {
-    event.preventDefault()
-    console.log('login with', username, password)
-  }  
+  const onSubmit = (event) => {
+    event.preventDefault();
+    handleLogin({ username, password });
+    setUsername("");
+    setPassword("");
+  };
 
   return (
-    <form onSubmit={handleLogin}>
+    <form onSubmit={onSubmit}>
       <div>
         <label>username</label>
-        <input type="text" value={username} name="Username" onChange={({target}) => setUsername(target.value)} />
+        <input
+          type="text"
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
       </div>
       <div>
         <label>password</label>
-        <input type="password" value={password} name="Password" onChange={({target}) => setPassword(target.value)} />
+        <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
       </div>
       <button type="submit">login</button>
-      </form>
-  )
-}
+    </form>
+  );
+};
 
-export default Login
+export default Login;
