6bab9838fe770ee3a7e9c2c4325bffc85b4ab53e
Manuel Ospina <mospina@protonmail.com>
2021-03-01 16:05:41 +0900

    Exercise 5.7

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,8 +1,37 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from "react";
 
-export default Blog
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false);
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  return (
+    <div style={blogStyle}>
+      {!visible ? (
+        <div>
+          {blog.title}
+          <button onClick={() => setVisible(!visible)}>view</button>
+        </div>
+      ) : (
+        <div>
+          {blog.title}
+          <button onClick={() => setVisible(!visible)}>hide</button>
+          <br />
+          {blog.url}
+          <br />
+          likes: {blog.likes} <button>like</button>
+          <br />
+          {blog.author}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Blog;
