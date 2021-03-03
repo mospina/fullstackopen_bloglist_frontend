38beda1e230327bae70ed60e43a21f82c526cde3
Manuel Ospina <mospina@protonmail.com>
2021-03-04 05:44:59 +0900

    Exercise 5.19

--- cypress/integration/blog_app.spec.js
+++ cypress/integration/blog_app.spec.js
@@ -1,35 +1,51 @@
-describe('Blog App', function () {
+describe("Blog App", function () {
   beforeEach(function () {
-    cy.request('POST', 'http://localhost:3001/api/testing/reset')
+    cy.request("POST", "http://localhost:3001/api/testing/reset");
     const user = {
-      name: 'Test User',
-      username: 'testuser',
-      password: 'secret',
-    }
-    cy.request('POST', 'http://localhost:3001/api/users', user)
-    cy.visit('http://localhost:3000')
-  })
+      name: "Test User",
+      username: "testuser",
+      password: "secret",
+    };
+    cy.request("POST", "http://localhost:3001/api/users", user);
+    cy.visit("http://localhost:3000");
+  });
 
-  it('Login form is shown', function () {
-    cy.contains('username')
-    cy.contains('password')
-    cy.contains('login')
-  })
+  it("Login form is shown", function () {
+    cy.contains("username");
+    cy.contains("password");
+    cy.contains("login");
+  });
 
-  describe('Login', function () {
-    it('succeeds with correct credentials', function () {
-      cy.get('#username').type('testuser')
-      cy.get('#password').type('secret')
-      cy.get('#login-button').click()
+  describe("Login", function () {
+    it("succeeds with correct credentials", function () {
+      cy.get("#username").type("testuser");
+      cy.get("#password").type("secret");
+      cy.get("#login-button").click();
 
-      cy.contains('logout')
-    })
-    it('fails with wrong credentials', function () {
-      cy.get('#username').type('testuser')
-      cy.get('#password').type('wrong')
-      cy.get('#login-button').click()
+      cy.contains("logout");
+    });
+    it("fails with wrong credentials", function () {
+      cy.get("#username").type("testuser");
+      cy.get("#password").type("wrong");
+      cy.get("#login-button").click();
 
-      cy.contains('Wrong Credentials')
-    })
-  })
-})
+      cy.contains("Wrong Credentials");
+    });
+  });
+
+  describe("When Logged in", function () {
+    beforeEach(function () {
+      cy.login({ username: "testuser", password: "secret" });
+    });
+
+    it("a blog can be created", function () {
+      cy.contains("new blog").click();
+      cy.get("#title").type("a blog created by cypress");
+      cy.get("#author").type("author");
+      cy.get("#url").type("http://url.url");
+      cy.contains("Submit").click();
+
+      cy.contains("a blog created by cypress");
+    });
+  });
+});
