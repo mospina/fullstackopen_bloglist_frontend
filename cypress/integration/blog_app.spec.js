4c0596c3e0ba632e782f6ef12652090c471e5d6c
Manuel Ospina <mospina@protonmail.com>
2021-03-04 23:30:30 +0900

    Exercise 5.22

--- cypress/integration/blog_app.spec.js
+++ cypress/integration/blog_app.spec.js
@@ -1,74 +1,116 @@
-describe("Blog App", function () {
+describe('Blog App', function () {
   beforeEach(function () {
-    cy.request("POST", "http://localhost:3001/api/testing/reset");
+    cy.request('POST', 'http://localhost:3001/api/testing/reset')
     const user = {
-      name: "Test User",
-      username: "testuser",
-      password: "secret",
-    };
-    cy.request("POST", "http://localhost:3001/api/users", user);
-    cy.visit("http://localhost:3000");
-  });
+      name: 'Test User',
+      username: 'testuser',
+      password: 'secret',
+    }
+    cy.request('POST', 'http://localhost:3001/api/users', user)
+    cy.visit('http://localhost:3000')
+  })
 
-  it("Login form is shown", function () {
-    cy.contains("username");
-    cy.contains("password");
-    cy.contains("login");
-  });
+  it('Login form is shown', function () {
+    cy.contains('username')
+    cy.contains('password')
+    cy.contains('login')
+  })
 
-  describe("Login", function () {
-    it("succeeds with correct credentials", function () {
-      cy.get("#username").type("testuser");
-      cy.get("#password").type("secret");
-      cy.get("#login-button").click();
+  describe('Login', function () {
+    it('succeeds with correct credentials', function () {
+      cy.get('#username').type('testuser')
+      cy.get('#password').type('secret')
+      cy.get('#login-button').click()
 
-      cy.contains("logout");
-    });
-    it("fails with wrong credentials", function () {
-      cy.get("#username").type("testuser");
-      cy.get("#password").type("wrong");
-      cy.get("#login-button").click();
+      cy.contains('logout')
+    })
+    it('fails with wrong credentials', function () {
+      cy.get('#username').type('testuser')
+      cy.get('#password').type('wrong')
+      cy.get('#login-button').click()
 
-      cy.contains("Wrong Credentials");
-    });
-  });
+      cy.contains('Wrong Credentials')
+    })
+  })
 
-  describe("When Logged in", function () {
+  describe('When Logged in', function () {
     beforeEach(function () {
-      cy.login({ username: "testuser", password: "secret" });
-    });
+      cy.login({ username: 'testuser', password: 'secret' })
+    })
 
-    it("a blog can be created", function () {
-      cy.contains("new blog").click();
-      cy.get("#title").type("a blog created by cypress");
-      cy.get("#author").type("author");
-      cy.get("#url").type("http://url.url");
-      cy.contains("Submit").click();
+    it('a blog can be created', function () {
+      cy.contains('new blog').click()
+      cy.get('#title').type('a blog created by cypress')
+      cy.get('#author').type('author')
+      cy.get('#url').type('http://url.url')
+      cy.contains('Submit').click()
 
-      cy.contains("a blog created by cypress");
-    });
+      cy.contains('a blog created by cypress')
+    })
 
-    describe("and a blog exists", function () {
+    describe('and a blog exists', function () {
       beforeEach(function () {
         cy.createBlog({
-          title: "an existing blog",
-          author: "author",
-          url: "http://example.com",
-        });
-      });
-      it("can be liked", function () {
-        cy.contains("an existing blog").contains("view").click();
-        cy.contains("like").click();
+          title: 'an existing blog',
+          author: 'author',
+          url: 'http://example.com',
+        })
+      })
+      it('can be liked', function () {
+        cy.contains('an existing blog').contains('view').click()
+        cy.contains('like').click()
 
-        cy.contains("likes: 1");
-      });
+        cy.contains('likes: 1')
+      })
 
-      it("can be deleted by owner", function () {
-        cy.contains("an existing blog").contains("view").click();
-        cy.contains("remove").click();
+      it('can be deleted by owner', function () {
+        cy.contains('an existing blog').contains('view').click()
+        cy.contains('remove').click()
 
-        cy.get("#blog-list").children().should("have.length", 0);
-      });
-    });
-  });
-});
+        cy.get('#blog-list').children().should('have.length', 0)
+      })
+    })
+
+    describe('and several blogs exist', function () {
+      beforeEach(function () {
+        cy.createBlog({
+          title: 'first blog',
+          author: 'author',
+          url: 'http://example.com',
+        })
+        cy.createBlog({
+          title: 'second blog',
+          author: 'author',
+          url: 'http://example.com',
+        })
+        cy.createBlog({
+          title: 'third blog',
+          author: 'author',
+          url: 'http://example.com',
+        })
+      })
+
+      it('are ordered by likes', function () {
+        cy.contains('second blog').contains('view').click()
+        cy.contains('like').click()
+        cy.contains('like').click()
+        cy.contains('hide').click()
+
+        cy.contains('third blog').contains('view').click()
+        cy.contains('like').click()
+        cy.contains('hide').click()
+
+        cy.get('#blog-list')
+          .children()
+          .then((children) => {
+            const expected = ['second blog', 'third blog', 'first blog']
+            return [...children].every(
+              (child, i) =>
+                child.textContent.trim().replace('view', '') === expected[i]
+            )
+          })
+          .should('equals', true)
+      })
+    })
+  })
+})
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,27 +23,27 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
-Cypress.Commands.add("login", ({ username, password }) => {
-  cy.request("POST", "http://localhost:3001/api/login", {
+Cypress.Commands.add('login', ({ username, password }) => {
+  cy.request('POST', 'http://localhost:3001/api/login', {
     username,
     password,
   }).then(({ body }) => {
-    localStorage.setItem("loggedBlogappUser", JSON.stringify(body));
-    cy.visit("http://localhost:3000");
-  });
-});
+    localStorage.setItem('loggedBlogappUser', JSON.stringify(body))
+    cy.visit('http://localhost:3000')
+  })
+})
 
-Cypress.Commands.add("createBlog", ({ title, author, url }) => {
+Cypress.Commands.add('createBlog', ({ title, author, url }) => {
   cy.request({
-    method: "POST",
-    url: "http://localhost:3001/api/blogs",
+    method: 'POST',
+    url: 'http://localhost:3001/api/blogs',
     body: { title, author, url },
     headers: {
       Authorization: `bearer ${
-        JSON.parse(localStorage.getItem("loggedBlogappUser")).token
+        JSON.parse(localStorage.getItem('loggedBlogappUser')).token
       }`,
     },
-  });
+  })
 
-  cy.visit("http://localhost:3000");
-});
+  cy.visit('http://localhost:3000')
+})
