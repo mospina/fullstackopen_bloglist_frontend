7e9ea32a3960363ea23b7d56dd464a141cd9734d
Manuel Ospina <manuel@nomoss.co>
2021-06-10 07:09:18 +0900

    Add users component with reducer and service

---
+++ src/components/Users.js
@@ -0,0 +1,26 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { initializeUsers } from "../reducers/usersReducer";
+
+const Users = () => {
+  const users = useSelector(({ users }) => users);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(initializeUsers());
+  }, [dispatch]);
+
+  return (
+    <div>
+      <h3>Users</h3>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Users;
---
+++ src/reducers/usersReducer.js
@@ -0,0 +1,31 @@
+/*
+ * type Users = User[]
+ * type User = {
+ *   name: string
+ *   username: string
+ *   blogs: Blog[]
+ * }
+ */
+
+import userService from "../services/users";
+
+const usersReducer = (state = [], action) => {
+  switch (action.type) {
+    case "INIT_USERS":
+      return action.data;
+    default:
+      return state;
+  }
+};
+
+export const initializeUsers = () => {
+  return async (dispatch) => {
+    const users = await userService.getAll();
+    dispatch({
+      type: "INIT_USERS",
+      data: users,
+    });
+  };
+};
+
+export default usersReducer;
